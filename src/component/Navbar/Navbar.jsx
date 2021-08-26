e50b47c12cbb5fc414f689ca2fac3b8be942ea43
Amitkundu-Eshan <86568866+Amitkundu-Eshan@users.noreply.github.com>
2021-08-26 12:04:26 +0530

    Update Navbar.jsx

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,11 +1,24 @@
-import React, {  } from 'react'
+import React, { useState } from 'react'
 import "./Navbar.scss"
 import { FaBars } from 'react-icons/fa';
 import {motion} from "framer-motion";
 
 const Navbar = ({menuopen,setMenuopen}) => {
-
-
+    
+    const [menuState,changeState]=useState({
+        ActiveObject:null,
+        Objects:[{id:0,name:"Home"},{id:1,name:"About"},{id:2,name:"Projects"},{id:3,name:"Contact"}]
+    });
+    const toggoleActive = (index) =>{
+        changeState({...menuState,ActiveObject:menuState.Objects[index.id]});
+    }
+    const toggleactiveStyle = (index) =>{
+        if(menuState.Objects[index.id] === menuState.ActiveObject){
+            return "bt active";
+        }else{
+            return "bt inactive"
+        }
+    }
 
     return (
         <>
@@ -16,11 +29,14 @@ const Navbar = ({menuopen,setMenuopen}) => {
             
             <div className="nav-menu">
             <FaBars  className="bar" onClick={()=>setMenuopen(!menuopen)}/>
-                <ul className="nav-items">
-                    <a style={{textDecoration:"none",color:"inherit"}} href="#Home"><li id="myDiv" className="nav-item">Home</li></a>
-                    <a style={{textDecoration:"none",color:"inherit"}} href="#About"><li  className="nav-item">About</li></a>
-                    <a style={{textDecoration:"none",color:"inherit"}} href="#Projects"><li  className="nav-item">Projects</li></a>
-                    <a style={{textDecoration:"none",color:"inherit"}} href="#Contact"><li  className="nav-item">Contact</li></a>
+                <ul className="nav-items" id="menu">
+                {menuState.Objects.map((index)=>(
+                    <a 
+                    key={index} 
+                    className={toggleactiveStyle(index)}  
+                    href={`#${index.name}`} 
+                    onClick={()=>{toggoleActive(index)}}>{index.name}</a>
+                ))}
                 </ul>
             </div>
         </div>
